3c1beb76121895d36dc816f4e1b9b8556c61d535
youko-wakou <appu2pudding@gmail.com>
2017-11-27 21:33:21 +0900

    修正MainActivity＆Anmal java

--- app/src/main/java/techacademy/wakou/youko/javalog/Animal.java
+++ app/src/main/java/techacademy/wakou/youko/javalog/Animal.java
@@ -3,6 +3,20 @@ package techacademy.wakou.youko.javalog;
 /**
  * Created by appu2 on 2017/11/27.
  */
+import android.util.Log;
+class Dog extends Animal {
+    static String to_jp = "犬";
+    public Dog(String name,int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public static void introduce() {
+        Log.d("javatest","これは犬クラスです");
+    }
+    public void say() {
+        Log.d("javatest",this.name + "("+ this.age + "歳)" + "「ワンワン」");
+    }
+}
 abstract class Animal {
     String name;
     int age;
